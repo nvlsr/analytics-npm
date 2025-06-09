69d2caabf044ca7cba33cee97574da4a4ca7f385
nvlsr <maaakri@yahoo.com>
2025-06-09 12:24:38 -0700

    udpate

--- analytics.tsx
+++ analytics.tsx
@@ -1,11 +1,18 @@
 "use client";
 
 import React from "react";
-import {
-  AnalyticsProvider,
-  type VisitorTrackerProps,
-} from "./analytics-provider";
+import dynamic from "next/dynamic";
+import type { VisitorTrackerProps } from "./analytics-provider";
 import type { ParsedAnalyticsHeaders } from "./server/header-parser";
+
+// Dynamically import the AnalyticsProvider to prevent SSR issues
+const AnalyticsProvider = dynamic(
+  () =>
+    import("./analytics-provider").then((mod) => ({
+      default: mod.AnalyticsProvider,
+    })),
+  { ssr: false }
+);
 
 export interface AnalyticsProps {
   analyticsData: ParsedAnalyticsHeaders;
